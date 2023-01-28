ad754a24489c3b4ccf113acb4323cf6910b28669
Дмитрий Маклаков <dimonabinsk@yandex.ru>
2023-01-28 23:26:33 +0300

    Чтение из файла

--- notes.controller.js
+++ notes.controller.js
@@ -1,17 +1,28 @@
 const fs = require("fs/promises");
+const path = require("path");
+
+const notesPath = path.join(__dirname, "db.json");
 
 async function addNote(title) {
-  const notes = require("./db.json");
+  //   const notes = require("./db.json");
+  const notes = await getNotes();
   const note = {
     title,
     id: Date.now().toString(),
   };
-    notes.push(note);
-    await fs.writeFile("./db.json", JSON.stringify(notes));
+  notes.push(note);
+  await fs.writeFile(notesPath, JSON.stringify(notes));
+
+  // const buffer = await fs.readFile(notesPath);
+  // const notes = Buffer.from(buffer).toString("utf-8");
+
 }
 
-function getNotes() {
-  return require("./db.json");
+addNote("Test!");
+
+async function getNotes() {
+  const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
 }
 
 module.exports = {
